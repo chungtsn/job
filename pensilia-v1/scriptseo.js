c7831944684c3d6f609182a53995b372a0a4f452
chungtsn <75374965+chungtsn@users.noreply.github.com>
2021-08-26 01:50:24 +0700

    Update scriptseo.js

--- pensilia-v1/scriptseo.js
+++ pensilia-v1/scriptseo.js
@@ -24,7 +24,7 @@ window.onload = function() {
 };
 function loadIds() {
 	for (let a = 0; a < checkboxesLength; a += 1) {
-		const b = a => a.replace(/[ ốểíóừ,.!?;:'-]/g, "");
+		const b = a => a.replace(/[ t,.!?;:'-]/g, "");
 		(checkboxes[a].id = `${b(
 			checkboxes[a].nextSibling.nextSibling.innerText
 		).toLowerCase()}`), checkboxes[a].nextSibling.setAttribute(
